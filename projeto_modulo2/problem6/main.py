0893f55123179348822d7bb9eed8874eda0c7af7
Andre Ramos Silva <andre-ramos1994@hotmail.com>
2021-01-31 11:10:50 -0300

    more exercises

---
+++ projeto_modulo2/problem6/main.py
@@ -0,0 +1,104 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    def __repr__(self):
+        return str(self.value)
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __str__(self):
+        cur_head = self.head
+        out_string = ""
+        while cur_head:
+            out_string += str(cur_head.value) + " -> "
+            cur_head = cur_head.next
+        return out_string
+
+    def append(self, value):
+
+        if self.head is None:
+            self.head = Node(value)
+            return
+
+        node = self.head
+        while node.next:
+            node = node.next
+
+        node.next = Node(value)
+
+    def size(self):
+        size = 0
+        node = self.head
+        while node:
+            size += 1
+            node = node.next
+
+        return size
+
+
+def ll_to_list(llist):
+    # O(n)
+    elements_list = []
+    current_element = llist.head
+    # getting values from
+    while current_element:
+        elements_list.append(current_element.value)
+        current_element = current_element.next
+    return elements_list
+
+
+def union(llist_1, llist_2):
+    list_1 = ll_to_list(llist_1)
+    list_2 = ll_to_list(llist_2)
+    elements_list = list_1 + list_2
+
+    elements_set = set(elements_list)
+    return elements_set
+
+
+def intersection(llist_1, llist_2):
+    list_1 = ll_to_list(llist_1)
+    list_2 = ll_to_list(llist_2)
+
+    return list(set(list_1) & set(list_2))
+
+
+
+# Test case 1
+
+linked_list_1 = LinkedList()
+linked_list_2 = LinkedList()
+
+element_1 = [3, 2, 4, 35, 6, 65, 6, 4, 3, 21]
+element_2 = [6, 32, 4, 9, 6, 1, 11, 21, 1]
+
+for i in element_1:
+    linked_list_1.append(i)
+
+for i in element_2:
+    linked_list_2.append(i)
+
+print (union(linked_list_1, linked_list_2))
+print (intersection(linked_list_1, linked_list_2))
+
+# Test case 2
+
+linked_list_3 = LinkedList()
+linked_list_4 = LinkedList()
+
+element_1 = [3, 2, 4, 35, 6, 65, 6, 4, 3, 23]
+element_2 = [1, 7, 8, 9, 11, 21, 1]
+
+for i in element_1:
+    linked_list_3.append(i)
+
+for i in element_2:
+    linked_list_4.append(i)
+
+print (union(linked_list_3, linked_list_4))
+print (intersection(linked_list_3, linked_list_4))
