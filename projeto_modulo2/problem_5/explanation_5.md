7f7bfe32ecd4489a36ccd33f176331aac6497974
Andre Ramos Silva <andre-ramos1994@hotmail.com>
2021-02-05 09:44:59 -0300

    problem 5

--- projeto_modulo2/problem_5/explanation_5.md
+++ projeto_modulo2/problem_5/explanation_5.md
@@ -1,11 +1,13 @@
 ### Design:
 <details about implementation such as data structures and algorithms used>
-i used a linked list type of structure to create this, to add a block or get , first or last block is o(1) but to find a specific block would be o(n)
+I used a linked list type of structure to create this, to add a block or get , first or last block is o(1) but to find a specific block would be o(n)
 
 ### Time Complexity:
 <Big O notation with brief explanation>
-to add a block or get , first or last block is o(1)
+To add a block or get , first or last block is o(1)
 but to find a specific block would be o(n)
+
+
 ### Space Complexity:
 <Big O notation with brief explanation>
 
--- projeto_modulo2/problem_5/problem_5.py
+++ projeto_modulo2/problem_5/problem_5.py
@@ -1,5 +1,5 @@
 import hashlib
-from datetime import datetime
+from _datetime import datetime
 
 
 
@@ -25,12 +25,87 @@ class BlockChain:
         self.head = None
         self.tail = None
 
-    def add_block(self,data):
+    def add_block(self, data):
+        now = datetime.today()
+
+        timestamp = datetime.timestamp(now)
+
         if self.head != None:
             prev = self.tail
-            self.tail = Block(datetime.today().strftime('%Y-%m-%d-%H:%M:%S'),data,prev)
+            self.tail = Block(timestamp, data, prev)
         else:
-            self.head = Block(datetime.today().strftime('%Y-%m-%d-%H:%M:%S'),data,0)
+            self.head = Block(timestamp, data, 0)
+            self.tail = self.head
+
+    def print_block_chain(self):
+        current_block = self.tail
+        index = 0
+        while current_block:
+            print("Index: ", index)
+            print("Timestamp: ", datetime.fromtimestamp(current_block.timestamp))
+            print("data: ", current_block.data)
+            print("SHA256 Hash: ", current_block.hash)
+            print("Prev Hash: ", current_block.previous_hash)
+            current_block = current_block.previous_hash
+            index += 1
+
+# test case 1 :
+new_block = BlockChain()
+new_block.add_block("Genesis")
+new_block.add_block("arbitrary")
+new_block.add_block("2")
+new_block.add_block("3")
+new_block.add_block("4")
+new_block.add_block("5")
+new_block.add_block("6")
+new_block.add_block("7")
+
+new_block.print_block_chain()
+
+#Example output:
+# Index:  0
+# Timestamp:  2021-02-05 09:42:36.457762
+# data:  7
+# SHA256 Hash:  a20200a94c75010576e2d6a83e6fa69271901a9d805894b28bd91e6054fbfd10
+# Prev Hash:  <__main__.Block object at 0x7f949a51f310>
+# Index:  1
+# Timestamp:  2021-02-05 09:42:36.457757
+# data:  6
+# SHA256 Hash:  a20200a94c75010576e2d6a83e6fa69271901a9d805894b28bd91e6054fbfd10
+# Prev Hash:  <__main__.Block object at 0x7f949a51f2b0>
+# Index:  2
+# Timestamp:  2021-02-05 09:42:36.457752
+# data:  5
+# SHA256 Hash:  a20200a94c75010576e2d6a83e6fa69271901a9d805894b28bd91e6054fbfd10
+# Prev Hash:  <__main__.Block object at 0x7f949a51f250>
+# Index:  3
+# Timestamp:  2021-02-05 09:42:36.457747
+# data:  4
+# SHA256 Hash:  a20200a94c75010576e2d6a83e6fa69271901a9d805894b28bd91e6054fbfd10
+# Prev Hash:  <__main__.Block object at 0x7f949a51f1f0>
+# Index:  4
+# Timestamp:  2021-02-05 09:42:36.457742
+# data:  3
+# SHA256 Hash:  a20200a94c75010576e2d6a83e6fa69271901a9d805894b28bd91e6054fbfd10
+# Prev Hash:  <__main__.Block object at 0x7f949a51f190>
+# Index:  5
+# Timestamp:  2021-02-05 09:42:36.457737
+# data:  2
+# SHA256 Hash:  a20200a94c75010576e2d6a83e6fa69271901a9d805894b28bd91e6054fbfd10
+# Prev Hash:  <__main__.Block object at 0x7f949a4cc310>
+# Index:  6
+# Timestamp:  2021-02-05 09:42:36.457729
+# data:  arbitrary
+# SHA256 Hash:  a20200a94c75010576e2d6a83e6fa69271901a9d805894b28bd91e6054fbfd10
+# Prev Hash:  <__main__.Block object at 0x7f949a4a0130>
+# Index:  7
+# Timestamp:  2021-02-05 09:42:36.457687
+# data:  Genesis
+# SHA256 Hash:  a20200a94c75010576e2d6a83e6fa69271901a9d805894b28bd91e6054fbfd10
+# Prev Hash:  0
 
 
-print(new_block.timestamp,new_block.data,new_block.previous_hash,new_block.hash)
+
+
+
+
