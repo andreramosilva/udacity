d16a83c5726a56d58d1a312cf809424e730a6d1b
Andre Ramos Silva <andre-ramos1994@hotmail.com>
2021-02-25 08:49:45 -0300

    fixing problem 1

--- projeto_modulo3/problem_1.py
+++ projeto_modulo3/problem_1.py
@@ -7,7 +7,19 @@ def sqrt(number):
     Returns:
        int: Floored Square Root
     """
-    return int(number**0.5)
+    # Base cases
+    if (number == 0 or number == 1):
+        return number
+
+        # Staring from 1, try all numbers until
+    # i*i is greater than or equal to x.
+    i = 1;
+    result = 1
+    while (result <= number):
+        i += 1
+        result = i * i
+
+    return i - 1
 
 print ("Pass" if  (3 == sqrt(9)) else "Fail")
 print ("Pass" if  (0 == sqrt(0)) else "Fail")
