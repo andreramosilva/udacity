9571956e415d7c1941833c23d50cd85e37aaa56e
Andre Ramos Silva <andre-ramos1994@hotmail.com>
2021-02-25 00:27:25 -0300

    bug fix on problem 7

--- projeto_modulo3/problem_7.py
+++ projeto_modulo3/problem_7.py
@@ -82,10 +82,11 @@ class Router:
 
 router = Router()  # remove the 'not found handler' if you did not implement this
 router.add_handler("/home/about", "about handler")  # add a route
+router.add_handler("/home/about/me", "about handler/me")  # add a route
 
 # some lookups with the expected output
 print(router.lookup("/"))  # should print 'root handler'
 print(router.lookup("/home"))  # should print 'not found handler' or None if you did not implement one
 print(router.lookup("/home/about"))  # should print 'about handler'
 print(router.lookup("/home/about/"))  # should print 'about handler' or None if you did not handle trailing slashes
-print(router.lookup("/home/about/me"))  # should print 'not found handler' or None if you did not implement one
+print(router.lookup("/home/about/me"))  # should print 'about handler/me'
